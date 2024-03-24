45f86426d2b4739273f22739a71be331da111710
Barto Molina <bartomolina@gmail.com>
2024-03-24 11:30:34 +0100

    feat: testing transfers

--- api/abi.ts
+++ api/abi.ts
@@ -1,25 +1,11 @@
 export const abi = [
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
     ],
     name: "approve",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
     stateMutability: "nonpayable",
     type: "function",
   },
